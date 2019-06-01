817646a59b34ff6d6b9637b87c2bc0f03e281869
Bevan Rudge <bevan@geneious.com>
2019-06-01 12:17:09 +1200

    Test script for coding task 1.

---
+++ task1.php
@@ -0,0 +1,11 @@
+<?php
+require('orm/DownloadLog.php');
+use Orm\DownloadLog;
+
+// This was copied from README.md.
+// New line characters were inserted at end of lines starting with `echo`.
+$downloadLog = DownloadLog::create();
+echo ($downloadLog->isModified() ? 'DownloadLog is modified' : 'DownloadLog is not modified') . "\n";
+$downloadLog->setFileId(1000)->setUserId(2000);
+echo ($downloadLog->isModified() ? 'DownloadLog is modified' : 'DownloadLog is not modified') . "\n";
+echo ("UserId is: " . $downloadLog->getUserId()) . "\n";
