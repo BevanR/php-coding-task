b8c6795e2c5b78d3afe40962fe2685a387e29a33
Bevan Rudge <bevan@geneious.com>
2019-06-02 16:56:05 +1200

    Use Guzzle's Response class instead

--- app/BaseController.php
+++ app/BaseController.php
@@ -2,16 +2,28 @@
 
 namespace app;
 
+use GuzzleHttp\Psr7\Response;
+use Orm\NumberValidator;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 abstract class BaseController
 {
+    // Stash headers for cookies etc here.
+    protected $headers = [];
+
     abstract function execute(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface;
 
-    function html(string $html, int $status = 200, string $reason = 'OK'): ResponseInterface {
-        $headers = [];
-        $headers['Content-Type'] = 'text/html; charset=utf-8';
-        return new Response($status, $reason, $headers, $html);
+    function html(string $html, int $status = null): ResponseInterface {
+        return $this->response('text/html; charset=utf-8', $html, $status);
+    }
+
+    function json(string $data, int $status = null): ResponseInterface {
+        return $this->response('application/json', json_encode($data), $status);
+    }
+
+    function response(string $type, string $body, int $status): ResponseInterface {
+        $this->headers['Content-Type'] = $type;
+        return new Response($status, $this->headers, $body);
     }
 }
